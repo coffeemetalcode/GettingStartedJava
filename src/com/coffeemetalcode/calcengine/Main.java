5482b31759975f65e08ca68465006c1d8eaba967
David Stocker <coffeemetalcode@gmail.com>
2020-12-16 21:20:11 -0500

    first commit

---
+++ src/com/coffeemetalcode/calcengine/Main.java
@@ -0,0 +1,9 @@
+package com.coffeemetalcode.calcengine;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("It lives!!!");
+    }
+}

3311324b9cd78c03457ad0f23eb428fab2dc5a0c
David Stocker <coffeemetalcode@gmail.com>
2020-12-16 22:18:12 -0500

    fresh

--- src/com/coffeemetalcode/calcengine/Main.java
+++ src/com/coffeemetalcode/calcengine/Main.java
@@ -8,40 +8,24 @@ public class Main {
 	    double result = 0.0d;
 	    char opCode = 's';
 
-        switch (opCode) {
-            case 'a':
-                result = value1 + value2;
-                break;
-            case 's':
-                result = value1 - value2;
-                break;
-            case 'm':
-                result = value1 * value2;
-                break;
-            case 'd':
-                result = (value2 != 0) ? value1 / value2 : 0.0d;
-                break;
-            default:
-                System.out.println("Invalid opCode: " + opCode);
-                result = 0.0d;
-                break;
+        if (opCode == 'a') {
+            result = value1 + value2;
         }
-        System.out.println(result);
-	    /*
-	    if (opCode == 'a')
-	        result = value1 + value2;
-	    else if (opCode == 's')
-	        result = value1 - value2;
-	    else if (opCode == 'm')
-	        result = value1 * value2;
+	    else if (opCode == 's') {
+            result = value1 - value2;
+        }
+	    else if (opCode == 'm') {
+            result = value1 * value2;
+        }
 	    else if (opCode == 'd') {
-            if (value2 != 0)
+            if (value2 != 0) {
                 result = value1 / value2;
+            }
         }
 	    else {
             System.out.println("Invalid opCode: " + opCode);
             result = 0.0d;
         }
-	     */
+        System.out.println(result);
     }
 }

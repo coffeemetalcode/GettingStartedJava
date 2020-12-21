30010e71a175ead30378001653f326bc2bba2f74
David Stocker <coffeemetalcode@gmail.com>
2020-12-21 01:39:01 -0500

    interactive mode

--- src/com/coffeemetalcode/calcengine/Main.java
+++ src/com/coffeemetalcode/calcengine/Main.java
@@ -1,5 +1,7 @@
 package com.coffeemetalcode.calcengine;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -15,11 +17,32 @@ public class Main {
             for (double currentResult : results) {
                 System.out.println(currentResult);
             }
+        } else if (args.length == 1 && args[0].equals("interactive")) {
+            executeInteractively();
         } else if (args.length == 3) {
             handleCommandLine(args);
         } else {
             System.out.println("Please provide an operation code and two numeric values.");
         }
+    }
+
+    static void executeInteractively() {
+        System.out.println("Please enter an operation and two numbers");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split(" ");
+        
+        performOperation(parts);
+    }
+
+    private static void performOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+
+        double result = execute(opCode, leftVal, rightVal);
+
+        System.out.println(result);
     }
 
     private static void handleCommandLine(String[] args) {
@@ -54,5 +77,26 @@ public class Main {
                 break;
         }
         return result;
+    }
+
+    static char opCodeFromString(String opCodeStr) {
+        char opCode = opCodeStr.toLowerCase().charAt(0);
+        return opCode;
+    }
+
+    static double valueFromWord(String word) {
+        String[] numberWords = {
+                "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+        };
+
+        double value = 0d;
+
+        for (int index = 0; index < numberWords.length; index++) {
+            if (word.equals(numberWords[index])) {
+                value = index;
+                break;
+            }
+        }
+        return value;
     }
 }

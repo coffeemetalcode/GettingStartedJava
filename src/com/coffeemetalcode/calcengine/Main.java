049a8e4e360e332e6837c51457f5879eca3fe55d
David B. Stocker <coffeemetalcode@gmail.com>
2021-01-05 21:32:24 -0500

    finish string-formatting module

--- src/com/coffeemetalcode/calcengine/Main.java
+++ src/com/coffeemetalcode/calcengine/Main.java
@@ -48,17 +48,9 @@ public class Main {
 
     private static void displayResult(char opCode, double leftVal, double rightVal, double result) {
         char symbol = symbolFromOpCode(opCode);
-        StringBuilder builder = new StringBuilder(45);
-
-        builder.append(leftVal);
-        builder.append(" ");
-        builder.append(symbol);
-        builder.append(" ");
-        builder.append(rightVal);
-        builder.append(" = ");
-        builder.append(result);
-
-        System.out.println(builder.toString());
+        String output = String.format("%.3f %c %.3f = %.3f", leftVal, symbol, rightVal, result);
+        
+        System.out.println(output);
     }
 
     private static char symbolFromOpCode(char opCode) {
